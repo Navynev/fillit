8814c6ebebcdf95bba2c7f41cbdac43d9f846cec
Jean-yves BRISSET <jbrisset@e2r13p1.42.fr>
2019-01-02 13:11:04 +0100

    check_tetriminos done, il faut le rendre propre et le refactoriser

--- check_tetriminos.c
+++ check_tetriminos.c
@@ -6,7 +6,7 @@
 /*   By: ndelhomm <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/28 11:52:15 by ndelhomm          #+#    #+#             */
-/*   Updated: 2018/12/30 17:44:15 by ndelhomm         ###   ########.fr       */
+/*   Updated: 2019/01/02 13:06:48 by jbrisset         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,12 @@
 #include <stdio.h>
 #include <fcntl.h>
 
-
 /*
  ** >> This fct's goal is to check if each (#) of the Tetrimino
  ** is adjacent to another (#).
  ** >> We also check if the number of points of contact between each (#) :
  ** is weather 3 (shapes: S, Z, T, J, L, I),
  ** or 4 (a square shape)
- **
- ** >> Le but de cette fct est de checker si chaque (#) du Tetrimino
- ** est adjacent a un autre (#).
- ** >> On va egalement verifier que le nombre de points de contact est :
- ** soit 3 (formes : S, Z, T J, L I), 
- ** soit 4 (forme carre)
- **
  */
 
 int	hashtag_adjacent(char *tetrimino)
@@ -37,7 +29,6 @@ int	hashtag_adjacent(char *tetrimino)
 
 	i = 0;
 	contact = 0;
-	//printf("[TETRIMINO = %s]\n", tetrimino);
 	while (i < 20)
 	{
 		if (tetrimino[i] == '#')
@@ -59,13 +50,9 @@ int	hashtag_adjacent(char *tetrimino)
 /*
  ** >> This fct's goal is to check the conformity of each piece:
  ** 4 lines, 4 (#), 3 (\n) and 12 (.)
- **
- ** >> Le but de cette fct est de checker si chaque piece est conforme :
- ** 4 lignes, 4 (#), 3 (\n) et 12 (.)
- **
  */
 
-int	piece_conformity(char *tetrimino)
+int		piece_conformity(char *tetrimino)
 {
 	int i;
 	int hashtag;
@@ -75,7 +62,7 @@ int	piece_conformity(char *tetrimino)
 	hashtag = 0;
 	line = 0;
 	while ((tetrimino[i] == '#' || tetrimino[i] == '.' || tetrimino[i] == '\n')
-			&& tetrimino[i] != '\0' && i < 21)
+		  && tetrimino[i] != '\0' && i < 21)
 	{
 		if (tetrimino[i] == '#')
 			hashtag++;
@@ -88,32 +75,93 @@ int	piece_conformity(char *tetrimino)
 	return (0);
 }
 
+/*
+  Check if file contains blocks made of 4 lines with only one '\n'
+  between them.
+ */
+int		check_general_conformity(int fd)
+{
+	char	*line;
+	int		res;
+	int		i;
+
+	i = 1;
+	line = ft_strdup("");
+	while ((res = get_next_line(fd, &line)) > 0)
+	{
+		if ((i % 5 == 0) && line[0] != 0)
+			return (0);
+		if ((i % 5 != 0) && ft_strlen(line) != 4)
+			return (0);
+		++i;
+		if (i > 130)
+			return (0);
+	}
+	printf("%d block(s)\n", i/5);
+	return (i / 5);
+}
+
+char	**get_megablock(int fd, int block_nb)
+{
+	char	**tetris;
+	char	*block;
+	int		i;
+	int		j;
+	char	*line;
+	char	*tmp;
+
+	tetris = (char **)malloc(sizeof(char *) * block_nb);
+	j = 0;
+	i = 1;
+	line = ft_strdup("");
+	while (j < (block_nb))
+	{
+		block = ft_strdup("");
+		while ((get_next_line(fd, &line) > 0) && (i % 5 != 0))
+		{
+			tmp = block;
+			line = ft_strjoin(line, "\n");
+			block = ft_strjoin(block, line);
+			free(tmp);
+			i++;
+		}
+		tetris[j] = ft_strdup(block);
+		free(block);
+		i++;
+		j++;
+	}
+	return (tetris);
+}
 
 int		main(int argc, char **argv)
 {
 	int		fd;
 	char	*line;
-	int		ret;
 	char	*tetri;
-	char	*tmp_tetri;
-	char	*tmp_line;
+	int		block_nb;
+	char	**megablock;
+	int		i;
 
+	i = 0;
 	(void)argc;
 	line = ft_strdup("");
 	tetri = ft_strdup("");
 	fd = open((argv[1]), O_RDONLY);
-	while ((ret = get_next_line(fd, &line)) > 0)
+	if (!(block_nb = check_general_conformity(fd)))
 	{
-		line = ft_strjoin(line, "\n");
-		//tmp_tetri = tetri;
-		tetri = ft_strjoin(tetri, line);
-		//free(tetri);
+		printf("error in check_general_conformity\n");
+		return (0);
 	}
-	printf("[TETRI = \n%s]\n", tetri);
-	printf("YOLO\n");
-	if (piece_conformity(tetri))
-		printf("Tetrimino is valid\n");
-	else
-		printf("Tetrimino is not valid\n");
+	close(fd);
+	fd = open((argv[1]), O_RDONLY);
+	megablock = get_megablock(fd, block_nb);
+	while (i < block_nb)
+	{
+		if (piece_conformity(megablock[i]))
+			printf("Tetrimino %d is valid\n", i + 1);
+		else
+			printf("Tetrimino %d is not valid\n", i + 1);
+		i++;
+	}
 	return (0);
 }
